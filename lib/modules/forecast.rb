5dbecedd9d241e2a92b384dad37ebb794d7c4db0
John Akers <johnakersemail@gmail.com>
2015-04-18 01:45:58 -0700

    added error handling for invalid location

--- lib/modules/forecast.rb
+++ lib/modules/forecast.rb
@@ -12,6 +12,7 @@ class Forecast
     # 'location' is a string (e.g. "San Francisco")
     def get_weather(location)
       coordinates = Geocoder.coordinates(location)
+      return nil if Forecast.location_invalid?(coordinates)
       lat, long, unknown  = coordinates[0], coordinates[1], "... I'm afraid I don't know"
       HTTParty.get("https://api.forecast.io/forecast/#{Forecast.key}/#{lat},#{long}")
     end
@@ -25,8 +26,13 @@ class Forecast
 
     def get(location)
       response = Forecast.get_weather(location)
+      return "I do not know that location" if response.nil?        
       results  = Forecast.parse_weather(response)
       "currently the weather is #{results[:currently]}. It will be #{results[:hourly]} and #{results[:daily]}"
+    end
+
+    def location_invalid?(geocoder_response)
+      true if geocoder_response.nil?
     end
 
   end # self
